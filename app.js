18875ecde42602448454aa76b4443f07dafdb364
fabiochidda <fabio.chidda99@gmail.com>
2022-02-06 14:58:42 +0100

    Inseriti i prompt per leggere il numero di chilometri e l'età del passeggero

---
+++ app.js
@@ -0,0 +1,14 @@
+// Alert di benvenuto
+alert("Benvenuto")
+
+// Prompt per inserire il numero di chilometri
+
+let userKm = prompt("Inserisci qui i chilometri che vuoi percorrere");
+
+console.log(userKm);
+
+// Prompt per inserire l'età del passeggero
+
+let passengerAge = prompt("Inserisci qui l'età del passeggero");
+
+console.log(passengerAge);
